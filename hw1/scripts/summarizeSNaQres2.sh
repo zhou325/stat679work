ee24b4dc9af1e2abbbfbf2b9f88ba52c6e45eba7
zhou325 <zhou325@wisc.edu>
2016-10-09 15:42:11 -0500

    delete some useless files

--- hw1/scripts/summarizeSNaQres2.sh
+++
@@ -1,8 +0,0 @@
-echo "analysis", "hmax", "CPU time" >> results/summarizeSNaQres2.csv
-for filename in hw1-snaqTimeTests/log/*.log
-do
-  a=`echo $filename | grep -Eo "/\w+\." | grep -Eo "\w+"` #get the file name root
-  b=`grep "hmax =" $filename | grep -Eo "\d+"` #get hmax
-  c=`cat hw1-snaqTimeTests/out/$a.out | grep -Eo "Elapsed time: \d+" | grep -Eo "\d+"` #get CPU time
-  echo $a,$b,$c >> results/summarizeSNaQres2.csv
-done
