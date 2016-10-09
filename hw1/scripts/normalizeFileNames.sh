3c979d004c14040566cf420fe0f6ace89035f3a4
zhou325 <zhou325@wisc.edu>
2016-10-09 15:43:41 -0500

    modify normalizeFileNames.sh

--- hw1/scripts/normalizeFileNames.sh
+++ hw1/scripts/normalizeFileNames.sh
@@ -1,11 +1,13 @@
-#change all file names timetesty_snaq.log to timetest0y_snaq.log
-for y in hw1-snaqTimeTests/log/*st[1,2,3,4,5,6,7,8,9]_snaq.log
+#change all file names timetesty_snaq.log to timetest0y_snaq.log, and all file names timetesty_snaq.out to timetest0y_snaq.out
+for i in {1..9}
 do
-mv ${y} ${y/timetest/timetest0}
+mv hw1-snaqTimeTests/log/timetest${i}_snaq.log hw1-snaqTimeTests/log/timetest0${i}_snaq.log
+mv hw1-snaqTimeTests/out/timetest${i}_snaq.out hw1-snaqTimeTests/out/timetest0${i}_snaq.out
 done
 
-#change all file names timetesty_snaq.out to timetest0y_snaq.out
-for y in hw1-snaqTimeTests/out/*st[1,2,3,4,5,6,7,8,9]_snaq.out
-do
-mv ${y} ${y/timetest/timetest0}
-done
+##undo this step
+# for i in {1..9}
+# do
+# mv hw1-snaqTimeTests/log/timetest0${i}_snaq.log hw1-snaqTimeTests/log/timetest${i}_snaq.log
+# mv hw1-snaqTimeTests/out/timetest0${i}_snaq.out hw1-snaqTimeTests/out/timetest${i}_snaq.out
+# done
